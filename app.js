5068a2e87c2db883a686532fb8ac0305c0116016
Marcus Couto <marcus.couto@m4u.com.br>
2017-08-15 09:22:30 -0300

    Arquivos iniciais

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = require('./config/server')
+
+var noticias = require('./app/routes/noticias')(app);
+var home = require('./app/routes/home')(app);
+var addNoticias = require('./app/routes/inclusao_noticias')(app);
+
+app.listen(3000, function(){
+	console.log("Server Running"); 	
+});
---
+++ mode_test.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+	var msg = "Texto do modulo de teste";
+	return msg;
+}
