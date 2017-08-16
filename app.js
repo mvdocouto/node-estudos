28415fb61cdd8d516d1058f0c954da24855ab6f4
Marcus Couto <marcus.couto@m4u.com.br>
2017-08-16 15:21:31 -0300

    inclusão das rotas atrvés do Consign.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('./config/server')
 
-var noticias = require('./app/routes/noticias')(app);
-var home = require('./app/routes/home')(app);
-var addNoticias = require('./app/routes/inclusao_noticias')(app);
+// var noticias = require('./app/routes/noticias')(app);
+// var home = require('./app/routes/home')(app);
+// var addNoticias = require('./app/routes/inclusao_noticias')(app);
 
 app.listen(3000, function(){
 	console.log("Server Running"); 	
--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
-var app = express();
+var consign = require('consign')
 
+var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+// Carregando todas as rotas automaticamente
+consign().include('app/routes').into(app);
 
 module.exports = app
